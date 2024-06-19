c6599c5d8fb098f61f7a245887bb50b9da5845f0
propy1113 <pypro1113@gmail.com>
2024-06-20 02:29:28 +0900

    フロントエンドのロギングの不要なコメントアウトを削除

--- frontend/pino.js
+++ frontend/pino.js
@@ -1,9 +1,4 @@
 const pino = require('pino');
-import axios from 'axios';
-
-// const fetch = async (obj) => {
-//   const response = await axios.post('http://0.0.0.0:8000/api/logs/', obj);
-// };
 
 // ブラウザ向け設定
 const logger = pino({
@@ -14,21 +9,8 @@ const logger = pino({
       send: function (level, logEvent) {
         // ここでログを送信する処理を実装する
         console.log(`Sending log to server: ${logEvent.message}`);
-        // サーバーにログを送信する処理を実装する
-        // fetch({
-        //   method: 'POST',
-        //   headers: {
-        //     'Content-Type': 'application/json',
-        //   },
-        //   body: JSON.stringify(logEvent),
-        // }).catch(error => console.error('Failed to send log to server', error));
       },
     },
-    // write: {
-    //   level: 'info', // ローカルに書き込むログのレベルを設定
-    //   timestamp: pino.stdTimeFunctions.isoTime,
-    //   destination: '/logs/app.log', // ローカルに保存するファイルのパス
-    // },
   },
 });
 
